94834acb561580ec13eed3640c8478948c4eb85b
Si Hyeong Lee <super2451894@gmail.com>
2018-12-14 13:50:20 +0900

    Update app.js
    - Remove users router because now it is useless
    - Add DB Connection Complete message

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +36,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+console.log(`MySQL DB에 성공적으로 연결됨!`);
 
 module.exports = app;
