d461d3b47b1fffe97c48bce88ffc7857de020347
Si Hyeong Lee <super2451894@gmail.com>
2018-12-15 21:34:10 +0900

    Update index.js
    - Add route pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// DB Import
+const db = require('../lib/db');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('login', {
+
+  });
+});
+
+
+// User Page
+router.get('/:userId', function (req, res, next) {
+  let userId = req.params.userId;
+  // GET Subject Data from DB
+  db.query(`SELECT * FROM SUBJECT_DATA`, function (error, data) {
+    if (error) {
+      throw error;
+    }
+
+    res.render('index', {
+      subjectData: data
+    });
+  });
+});
+
+router.post(`/:userId/submit`, function (req, res, next) {
+  let userId=req.params.userId;
+  
+  let reqProfessor = req.body.reqProfessor; // 교수님에게 개인 요청 사항
+  console.log(reqProfessor);
+  console.log('success');
+  res.redirect(`/${userId}`);
 });
 
 module.exports = router;
