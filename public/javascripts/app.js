39fcef25255e847e1a1edd657c7a0bf025c97dc5
Si Hyeong Lee <super2451894@gmail.com>
2018-12-15 16:17:07 +0900

    Update app.js
    - Fix Collision in Timeline
    - Overlap Check

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,10 +2,12 @@
 let sbjCodeArr = new Array();
 let count = 0;
 
+// 타임라인 중복과목 체크용 배열
+let checkCollision = new Array();
+
 /* 랜덤 컬러 생성 함수
 타임라인에서 사용하는 각각의 칸 색깔 생성
 */
-
 function getRamdomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -39,7 +41,6 @@ function selectList() {
     }
 }
 selectList();
-// Timeline Event 초기화
 
 
 // Add Subject Button Click Event
@@ -78,9 +79,54 @@ $('.checkBtn').click(function () {
     //     return '총 수강학점 : ' + count; // 21학점으로 초기화
     // }))
 
+    // 선택된 데이터 요일, 시간 배열생성 (요일 포함)
 
+    let changeday;
+    let changeday1;
+    switch (td.eq(4).text().substring(0, 3)) {
+        case '월요일':
+            changeday = 'mon';
+            break;
+        case '화요일':
+            changeday = 'tue';
+            break;
+        case '수요일':
+            changeday = 'wed';
+            break;
+        case '목요일':
+            changeday = 'tur';
+            break;
+        case '금요일':
+            changeday = 'fri';
+            break;
+    }
+    switch (td.eq(4).text().substring(4, 7)) {
+        case '월요일':
+            changeday1 = 'mon';
+            break;
+        case '화요일':
+            changeday1 = 'tue';
+            break;
+        case '수요일':
+            changeday1 = 'wed';
+            break;
+        case '목요일':
+            changeday1 = 'tur';
+            break;
+        case '금요일':
+            changeday1 = 'fri';
+            break;
+    }
 
-    //variable with the new value to be added in the array
+    let newDay = changeday + td.eq(5).text().substring(0, 1);
+    let newDay1 = changeday1 + td.eq(5).text().substring(2, 3);
+    let newDay2 = changeday + (Number(td.eq(5).text().substring(0, 1)) + 1);
+    let newDay3 = changeday1 + (Number(td.eq(5).text().substring(2, 3)) + 1);
+
+    if (checkCollision.indexOf(newDay) == -1 && checkCollision.indexOf(newDay1) == -1 && checkCollision.indexOf(newDay2) == -1 && checkCollision.indexOf(newDay3) == -1) {
+        checkCollision.push(newDay, newDay1, newDay2, newDay3);
+        console.log(checkCollision);
+        // 추가된 과목 중복검사
     let newPush = td.eq(0).text();
     //add if it is not in the array, and sends a warning if
     if (sbjCodeArr.indexOf(newPush) == -1) {
@@ -173,6 +219,104 @@ $('.checkBtn').click(function () {
     //throws the value of the new array in the console for verification
     console.log(sbjCodeArr);
 
+    } else {
+        alert('선택한 항목과 시간이 충돌합니다');
+        console.log(checkCollision);
+    }
+
+    // // 추가된 과목 중복검사
+    // let newPush = td.eq(0).text();
+    // //add if it is not in the array, and sends a warning if
+    // if (sbjCodeArr.indexOf(newPush) == -1) {
+    //     //If the array does not have this element
+    //     sbjCodeArr.push(newPush);
+
+    //     let subjectCode = row.insertCell(0);
+    //     let subjectName = row.insertCell(1);
+    //     let credit = row.insertCell(2);
+    //     let major = row.insertCell(3);
+    //     let day = row.insertCell(4);
+    //     let time = row.insertCell(5);
+    //     let deleteButton = row.insertCell(6);
+
+    //     // Select Tag의 선택된 데이터를 가져옴
+    //     let subject = td.eq(0).text();
+    //     let x = document.getElementById(subject).value;
+    //     $(`#${subject} option`).each(function () {
+    //         newDayArray.push($(this).val());
+    //     });
+    //     console.log(newDayArray);
+    //     $(`#${subject}Time option`).each(function () {
+    //         newTimeArray.push($(this).val());
+    //     });
+    //     console.log(newTimeArray);
+    //     // 선택된 데이터 배열 생성 (요일 포함)
+    //     let tdArr = [td.eq(0).text(), td.eq(1).text(), Number(td.eq(2).text()), td.eq(3).text(), newDayArray[0], newDayArray[1], newTimeArray[0], newTimeArray[1]];
+    //     console.log(tdArr);
+
+    //     // 선택된 수강항목 테이블에 삽입
+    //     subjectCode.innerHTML = td.eq(0).text();
+    //     subjectName.innerHTML = td.eq(1).text();
+    //     credit.innerHTML = Number(td.eq(2).text());
+    //     major.innerHTML = td.eq(3).text();
+    //     day.innerHTML = newDayArray[0] + ' ' + newDayArray[1];
+    //     time.innerHTML = td.eq(5).text();
+    //     deleteButton.innerHTML = `<button id="remove_button">삭제</button>`;
+
+    //     let clickedItem = td.eq(0).text();
+    //     console.log(clickedItem);
+    //     $('#' + clickedItem + ' option').each(function () {
+    //         dayArray.push($(this).val());
+    //     });
+    //     console.log(dayArray);
+    //     $('#' + clickedItem + 'Time option').each(function () {
+    //         timeArray.push($(this).val());
+    //     });
+    //     console.log(timeArray);
+
+    //     let timelineColor = getRamdomColor();
+
+    //     for (i = 0; i < dayArray.length; i++) {
+    //         let day;
+
+    //         switch (dayArray[i]) {
+    //             case '월요일':
+    //                 day = 'mon';
+    //                 break;
+    //             case '화요일':
+    //                 day = 'tue';
+    //                 break;
+    //             case '수요일':
+    //                 day = 'wed';
+    //                 break;
+    //             case '목요일':
+    //                 day = 'tur';
+    //                 break;
+    //             case '금요일':
+    //                 day = 'fri';
+    //                 break;
+    //         }
+
+    //         // I moved these variables into the loop too
+
+    //         let clickDay = day + timeArray[i];
+    //         let clickDay1 = day + (Number(timeArray[i]) + 1);
+
+    //         let table = document.getElementById(clickDay);
+    //         let table1 = document.getElementById(clickDay1);
+
+    //         table.style.backgroundColor = timelineColor;
+    //         table1.style.backgroundColor = timelineColor;
+    //     }
+    // } else {
+    //     //If the array already has this element
+    //     alert(`${newPush} 과목은 이미 수강신청 항목에 추가된 과목입니다.`)
+    //     // sbjCodeArr.pop(newPush);
+    // }
+
+    // //throws the value of the new array in the console for verification
+    // console.log(sbjCodeArr);
+
     // 삭제버튼 클릭시 수강항목 테이블에서 데이터 제거
     $('button').click(function () {
 
@@ -225,16 +369,14 @@ $('.checkBtn').click(function () {
                 break;
         }
 
-
+        // 삭제되는 테이블 ID값 출력
         console.log((day + clickTime) + ' ' + (day1 + clickTime1));
 
+        // 삭제된
         let firstTable = document.getElementById(day + clickTime);
-        console.log(firstTable);
         let firstTable1 = document.getElementById(day1 + Number(clickTime1));
-        console.log(firstTable1);
-
-        let secondTable=document.getElementById(day+(Number(clickTime)+1));
-        let secondTable1=document.getElementById(day1+(Number(clickTime1)+1));
+        let secondTable = document.getElementById(day + (Number(clickTime) + 1));
+        let secondTable1 = document.getElementById(day1 + (Number(clickTime1) + 1));
 
 
         firstTable.style.backgroundColor = '#fff';
